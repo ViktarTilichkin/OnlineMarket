57c16b10c529297090851633d5f8d6154e8a9922
Viktar Tilichkin <114764422+ViktarTilichkin@users.noreply.github.com>
2023-09-18 23:14:03 +0300

    Update Product.cs

--- OnlineMarket/Models/Repository/Product.cs
+++ OnlineMarket/Models/Repository/Product.cs
@@ -7,6 +7,6 @@
         public string Description { get; set; }
         public decimal Price { get; set; }
         public int Category_id { get; set; }
-        public int Brand_Id { get; set; }
+        public int Brand_Id { get; set; }    
     }
 }
